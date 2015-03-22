bae17b4d8684adb33cb5a6588906f1188765f95a
Jason Yang <jasonapeman@outlook.com>
2015-03-22 21:49:34 +0800

    HTML using Bootstrap=3=

---
+++ VoteHtml/JS/Elected.js
@@ -0,0 +1,13 @@
+var elected={
+    WuTao:{
+        name:"Wu Tao",
+        index:0,
+        description:"Tao Wu"
+    }
+    ,
+    LiZhuoli:{
+        name:"Li Zhuoli",
+        index:1,
+        description:"Zhuoli Li"
+    }
+}
